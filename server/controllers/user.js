65a9cb0d952ea86568b85938ce445102c8c7ef8b
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-22 18:18:27 +0300

    user now succesfully gets saved to mongoDB

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,9 +1,35 @@
+import mongoose from 'mongoose'
 import User from '../models/user.js'
 
 export const signup = (req, res) => {
+	// Get data from request
 	const { name, email, password } = req.body
+	// Check if all field are filled
 	if (!name || !email || !password) {
 		return res.status(422).json({ error: 'Please fill all the fields.' })
 	}
-	return res.status(201).json({ message: 'Succesfully sent.' })
+	// Check if user with that email already exists
+	User.findOne({ email })
+		.then((savedUser) => {
+			if (savedUser) {
+				return res.status(422).json({ error: 'User already exists with that email.' })
+			}
+			// Add data to model
+			const user = new User({
+				name,
+				email,
+				password,
+			})
+			// Save user
+			user.save()
+				.then((user) => {
+					res.status(201).json({ message: 'User succesfully saved.' })
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		})
+		.catch((err) => {
+			console.log(err)
+		})
 }
