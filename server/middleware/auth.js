026af0ea170ef2189b2932831ff997c41f87775a
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-23 02:07:13 +0300

    created middleware to check token for authentication

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user.js'
+
+export const auth = (req, res, next) => {
+	// Get authorization from request header
+	const { authorization } = req.headers
+	// Check if authorization is not there
+	if (!authorization) {
+		return res.status(401).json({ error: 'You are not logged in.' })
+	}
+	// Get just the token part
+	const token = authorization.replace('Bearer ', '')
+	// Verify that it's the same token
+	jwt.verify(token, process.env.JWT_SECRET, (error, payload) => {
+		if (error) {
+			return res.status(401).json({ error: 'You are not logged in.' })
+		}
+		// Get user by id
+		const { id } = payload
+		User.findById(id).then((userData) => {
+			req.user = userData
+		})
+		next()
+	})
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
 import express from 'express'
 
+import { auth } from '../middleware/auth.js'
 import { signin, signup } from '../controllers/user.js'
 
 // Setup router
 const router = express.Router()
+
+router.get('/needsauth', auth, (req, res) => {
+	res.send('YOU GOT IT')
+})
 
 // Routes
 router.post('/signup', signup)
