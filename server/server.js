c3cf1eb22c79e683248958ddcaa86a51fb35c861
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-22 17:34:41 +0300

    connection to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
-dotenv.config()
 // Initialize app
+dotenv.config()
 const app = express()
-// Create Port
 const PORT = process.env.PORT || 5000
-// Test route
-app.get('/', (req, res) => {
-	res.send('hello')
-})
+
+// Middleware
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_DB_CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
 // Listen
 app.listen(PORT, () => {
 	console.log(`Server is running on ${PORT}`)
